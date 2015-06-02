9d7126a1b5c480bd01af54da78da6fb5c3c7b407
Akash Agrawall <akash.agrawall094@gmail.com>
2015-06-02 13:37:54 +0530

    some new challenges

---
+++ hackerearth/juneeasychallenge2k15/cool.cpp
@@ -0,0 +1,120 @@
+/*******************
+  Akash Agrawall
+  IIIT HYDERABAD
+ ***********************/
+
+#include<cstdio>
+#include<iostream>
+#include<cstdlib>
+#include<cmath>
+#include<cstring>
+#include<climits>
+#include<algorithm>
+#include<vector>
+#include<set>
+#include<map>
+#include<bitset>
+#include<stack>
+#include<queue>
+#include<stdio.h>
+#include<string.h>
+#include<math.h>
+#include<functional>
+#include<numeric>
+using namespace std;
+#define FOR(i,a,b) for(i= a ; i < b ; ++i)
+#define rep(i,n) FOR(i,0,n)
+#define INF INT_MAX
+#define ALL(x) x.begin(),x.end()
+#define LET(x,a) __typeof(a) x(a)
+#define IFOR(i,a,b) for(LET(i,a);i!=(b);++i)
+#define EACH(it,v) IFOR(it,v.begin(),v.end())
+#define pb push_back
+#define sz(x) int(x.size())
+#define mp make_pair
+#define fill(x,v) memset(x,v,sizeof(x))
+#define max(a,b) ((a)>(b)?(a):(b))
+#define min(a,b) ((a)<(b)?(a):(b))
+#define si(n) scanf("%d",&n)
+#define pi(n) printf("%d ",n)
+#define pd(n) printf("%lf ",n);
+#define pdl(n) printf("%lf\n",n);
+#define pin(n) printf("%d\n",n)
+#define pln(n) printf("%lld\n",n)
+#define pl(n) printf("%lld ",n)
+#define sl(n) scanf("%lld",&n)
+#define sd(n) scanf("%lf",&n)
+#define ss(n) scanf("%s",n)
+#define scan(v,n) vector<int> v;rep(i,n){ int j;si(j);v.pb(j);}
+#define mod (int)(1e9 + 7)
+#define ll long long
+#define MAX 1000006
+#define TRACE
+
+#ifdef TRACE
+#define trace1(x)                cerr << #x << ": " << x << endl;
+#define trace2(x, y)             cerr << #x << ": " << x << " | " << #y << ": " << y << endl;
+#define trace3(x, y, z)          cerr << #x << ": " << x << " | " << #y << ": " << y << " | " << #z << ": " << z << endl;
+#define trace4(a, b, c, d)       cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << endl;
+#define trace5(a, b, c, d, e)    cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << " | " << #e << ": " << e << endl;
+#define trace6(a, b, c, d, e, f) cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << " | " << #e << ": " << e << " | " << #f << ": " << f << endl;
+
+#else
+
+#define trace1(x)
+#define trace2(x, y)
+#define trace3(x, y, z)
+#define trace4(a, b, c, d)
+#define trace5(a, b, c, d, e)
+#define trace6(a, b, c, d, e, f)
+
+#endif
+int dp[MAX],arr[MAX];
+int main()
+{
+	int t,r,k,i,mit,ans,j,cnt;
+	mit=100001;
+	i=1;
+	FOR(i,1,mit)
+	{
+		r=i;cnt=0;ans=0;
+		while(r!=0)
+		{
+			arr[cnt++]=r%2;
+			r/=2;
+		}
+		rep(j,cnt-2)
+		{
+			if(arr[j]==1 && arr[j+1]==0 && arr[j+2]==1)
+				ans++;
+		}
+		dp[i]=ans;
+	}
+	si(t);
+	while(t--)
+	{
+		ans=0;
+		si(r);
+		si(k);
+		FOR(i,1,r+1)
+		{
+			if(dp[i]>=k)
+				ans++;
+		}
+		pin(ans);
+	}
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
