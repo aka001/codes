1c81855b020003c42869971bda67b51f27d4ab25
Akash Agrawall <akash.agrawall094@gmail.com>
2015-08-24 14:51:16 +0530

    Easy DP

---
+++ codeforces/202div2/2.cpp
@@ -0,0 +1,111 @@
+/*******************
+  Akash Agrawall
+  IIIT HYDERABAD
+ ***********************/
+
+#include<bits/stdc++.h>
+using namespace std;
+#define FOR(i,a,b) for(i= a ; i < b ; ++i)
+#define rep(i,n) FOR(i,0,n)
+#define INF INT_MAX
+#define ALL(x) x.begin(),x.end()
+#define LET(x,a) __typeof(a) x(a)
+#define IFOR(i,a,b) for(LET(i,a);i!=(b);++i)
+#define EACH(it,v) IFOR(it,v.begin(),v.end())
+#define pb push_back
+#define sz(x) int(x.size())
+#define mp make_pair
+#define fill(x,v) memset(x,v,sizeof(x))
+#define max(a,b) ((a)>(b)?(a):(b))
+#define min(a,b) ((a)<(b)?(a):(b))
+#define si(n) scanf("%d",&n)
+#define pi(n) printf("%d ",n)
+#define pd(n) printf("%lf ",n);
+#define pdl(n) printf("%lf\n",n);
+#define pin(n) printf("%d\n",n)
+#define pln(n) printf("%lld\n",n)
+#define pl(n) printf("%lld ",n)
+#define sl(n) scanf("%lld",&n)
+#define sd(n) scanf("%lf",&n)
+#define ss(n) scanf("%s",n)
+#define scan(v,n) vector<int> v;rep(i,n){ int j;si(j);v.pb(j);}
+#define mod (int)(1e9 + 7)
+#define ll long long int
+#define MAX 1000006
+#define TRACE
+
+#ifdef TRACE
+#define trace1(x)                cerr << #x << ": " << x << endl;
+#define trace2(x, y)             cerr << #x << ": " << x << " | " << #y << ": " << y << endl;
+#define trace3(x, y, z)          cerr << #x << ": " << x << " | " << #y << ": " << y << " | " << #z << ": " << z << endl;
+#define trace4(a, b, c, d)       cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << endl;
+#define trace5(a, b, c, d, e)    cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << " | " << #e << ": " << e << endl;
+#define trace6(a, b, c, d, e, f) cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << " | " << #e << ": " << e << " | " << #f << ": " << f << endl;
+
+#else
+
+#define trace1(x)
+#define trace2(x, y)
+#define trace3(x, y, z)
+#define trace4(a, b, c, d)
+#define trace5(a, b, c, d, e)
+#define trace6(a, b, c, d, e, f)
+
+#endif
+ll modpow(ll a,ll n,ll temp){ll res=1,y=a;while(n>0){if(n&1)res=(res*y)%temp;y=(y*y)%temp;n/=2;}return res%temp;} 
+ll dp[MAX][12],val[20];
+vector<ll> fin;
+void find_answer(int v)
+{
+	int i=9,vit1=0,vit2;
+	for(i=9;i>0;i--)
+	{
+		if(dp[v][i]>vit1)
+		{
+			vit1=dp[v][i];
+			vit2=i;
+		}
+	}
+	if(vit1==0)
+		return;
+	fin.pb(vit2);
+	find_answer(v-val[vit2]);
+}
+int main()
+{
+	ll v,i,j,k,calc,ans,sz;
+	sl(v);
+	rep(i,9)
+		sl(val[i+1]);
+	rep(i,10)
+		dp[0][i]=0;
+	FOR(i,1,v+1)
+	{
+		FOR(j,1,10)
+		{
+			calc=i-val[j];
+			if(calc>=0)
+			{
+				ans=0;
+				for(k=9;k>=1;k--)
+				{
+					if(dp[calc][k]>ans)
+						ans=dp[calc][k];
+				}
+				dp[i][j]=1+ans;
+			}
+		}
+	}
+	find_answer(v);
+	sort(fin.rbegin(), fin.rend());
+	sz=fin.size();
+	if(sz==0)
+		pin(-1);
+	else
+	{
+		rep(i,sz)
+			printf("%lld",fin[i]);
+		printf("\n");
+	}
+	return 0;
+}
