a743a025ca92f80ec7381ef5547ed0d0e45559a2
Akash Agrawall <akash.agrawall094@gmail.com>
2015-08-26 17:45:54 +0530

    92div1 problem A: Number theory - length code

--- codeforces/92div1/1.cpp
+++ codeforces/92div1/1.cpp
@@ -52,11 +52,132 @@ using namespace std;
 #define trace6(a, b, c, d, e, f)
 
 #endif
+#define F first
+#define S second
 ll modpow(ll a,ll n,ll temp){ll res=1,y=a;while(n>0){if(n&1)res=(res*y)%temp;y=(y*y)%temp;n/=2;}return res%temp;} 
-
+char ch[MAX];
+int val[MAX], isprime[MAX], freq[MAX], fit[MAX];
+char allocate[MAX], disp[MAX];
+vector<int> primeit;
+set<pair<int,int> > stit;
+void seive()
+{
+	ll i,j;
+	isprime[0]=isprime[1]=1;
+	FOR(i,2,MAX)
+	{
+		if(isprime[i]==0)
+		{
+			j=i*i;
+			for(;j<MAX;j+=i)
+				isprime[j]=1;
+		}
+	}
+	FOR(i,2,MAX)
+	{
+		if(isprime[i]==0)
+			primeit.pb(i);
+	}
+}
 int main()
 {
-	
+	int i,sz,cnt,calc,cit=0,aka,a1,a2,mxit,sit,j;
+	char iiit;
+	seive();
+	ss(ch);
+	sz=strlen(ch);
+	i=0;
+	while(primeit[i]<=sz)
+	{
+		cit++;
+		calc=primeit[i];
+		cnt=1;
+		aka=0;
+		while(calc*cnt<=sz)
+		{
+			if(val[calc*cnt]!=0)
+			{
+				aka=val[calc*cnt];
+				break;
+			}
+			else
+				val[calc*cnt]=cit;
+			cnt++;
+		}
+		if(aka!=0)
+		{
+			calc=primeit[i];
+			cnt=1;
+			while(calc*cnt<=sz)
+			{
+				val[calc*cnt]=aka;
+				cnt++;
+			}
+			cit--;
+		}
+		i++;
+	}
+	FOR(i,1,sz+1)
+	{
+		fit[ch[i-1]]++;
+		if(val[i]==0)
+			continue;
+		if(stit.find(mp(freq[val[i]], val[i]))!=stit.end())
+		{
+			stit.erase(stit.find(mp(freq[val[i]], val[i])));
+		}
+		freq[val[i]]++;
+		stit.insert(mp(freq[val[i]], val[i]));
+		sit=stit.size();
+	}
+	pair<int,int> aaa;
+	while(!(stit.empty()))
+	{
+		aaa=*(stit.rbegin());
+		stit.erase(stit.find(*stit.rbegin()));
+		a1=aaa.F;
+		a2=aaa.S;
+		mxit= -1;
+		FOR(i,97,123)
+		{
+			if(fit[i]>mxit)
+			{
+				mxit=fit[i];
+				iiit=i;
+			}
+		}
+		if(mxit<a1)
+		{
+			printf("NO\n");
+			return 0;
+		}
+		allocate[a2]=iiit;
+		fit[iiit]-=a1;
+	}
+	string str="";
+	FOR(i,'a','z'+1)
+	{
+		if(fit[i]!=0)
+			str+=i;
+	}
+	FOR(i,1,sz+1)
+	{
+		FOR(j,1,sz+1)
+		{
+			if(val[j]==i)
+				disp[j]=allocate[val[j]];
+		}
+	}
+	cnt=0;
+	FOR(i,1,sz+1)
+	{
+		if(val[i]==0)
+			disp[i]=str[cnt++];
+	}
+	printf("YES\n");
+	FOR(i,1,sz+1)
+		cout<<disp[i];
+	cout<<endl;
 	return 0;
 }
 
