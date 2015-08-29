5e3b30441cc61260add3d496345bd3bd817b2998
Akash Agrawall <akash.agrawall094@gmail.com>
2015-08-29 17:22:31 +0530

    314div2 D: Using set lower bound. stit.lower_bound(val) returns an iterator to the >= value to val in the set. Use it to find the value lower than val in set. Return type of lower_bound is set<int>::iterator.

---
+++ codeforces/314div2/4.cpp
@@ -0,0 +1,97 @@
+/*******************
+  Akash Agrawall
+  IIIT HYDERABAD
+ ***********************/
+
+#include<bits/stdc++.h>
+using namespace std;
+#define FOR(i,a,b) for(i= a ; i < b ; ++i)
+#define rep(i,n) FOR(i,0,n)
+#define INF INT_MAX
+#define ALL(x) x.begin(),x.end()
+#define LET(x,a) __typeof(a) x(a)
+#define IFOR(i,a,b) for(LET(i,a);i!=(b);++i)
+#define EACH(it,v) IFOR(it,v.begin(),v.end())
+#define pb push_back
+#define sz(x) int(x.size())
+#define mp make_pair
+#define fill(x,v) memset(x,v,sizeof(x))
+#define max(a,b) ((a)>(b)?(a):(b))
+#define min(a,b) ((a)<(b)?(a):(b))
+#define si(n) scanf("%d",&n)
+#define pi(n) printf("%d ",n)
+#define pd(n) printf("%lf ",n);
+#define pdl(n) printf("%lf\n",n);
+#define pin(n) printf("%d\n",n)
+#define pln(n) printf("%lld\n",n)
+#define pl(n) printf("%lld ",n)
+#define sl(n) scanf("%lld",&n)
+#define sd(n) scanf("%lf",&n)
+#define ss(n) scanf("%s",n)
+#define scan(v,n) vector<int> v;rep(i,n){ int j;si(j);v.pb(j);}
+#define mod (int)(1e9 + 7)
+#define ll long long int
+#define MAX 1000006
+#define TRACE
+
+#ifdef TRACE
+#define trace1(x)                cerr << #x << ": " << x << endl;
+#define trace2(x, y)             cerr << #x << ": " << x << " | " << #y << ": " << y << endl;
+#define trace3(x, y, z)          cerr << #x << ": " << x << " | " << #y << ": " << y << " | " << #z << ": " << z << endl;
+#define trace4(a, b, c, d)       cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << endl;
+#define trace5(a, b, c, d, e)    cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << " | " << #e << ": " << e << endl;
+#define trace6(a, b, c, d, e, f) cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << " | " << #e << ": " << e << " | " << #f << ": " << f << endl;
+
+#else
+
+#define trace1(x)
+#define trace2(x, y)
+#define trace3(x, y, z)
+#define trace4(a, b, c, d)
+#define trace5(a, b, c, d, e)
+#define trace6(a, b, c, d, e, f)
+
+#endif
+ll modpow(ll a,ll n,ll temp){ll res=1,y=a;while(n>0){if(n&1)res=(res*y)%temp;y=(y*y)%temp;n/=2;}return res%temp;} 
+set<int> stit;
+int arr[MAX];
+int main()
+{
+	int n,k,a,m,ans,i,v1,v2;
+	si(n); si(k); si(a); si(m);
+	stit.insert(0);
+	stit.insert(n+1);
+	ans = (n+1)/(a+1);
+	set<int>::iterator itup;
+	rep(i,m)
+	{
+		si(arr[i]);
+		itup = stit.lower_bound(arr[i]);
+		v2=*itup;
+		itup--;
+		v1=*itup;
+		stit.insert(arr[i]);
+		ans-=(v2-v1)/(a+1);
+		ans= ans + (arr[i]-v1)/(a+1) + (v2-arr[i])/(a+1);
+		if(ans<k)
+		{
+			pin(i+1);
+			return 0;
+		}
+	}
+	pin(-1);
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
