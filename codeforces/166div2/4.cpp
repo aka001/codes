f0a2fba3e387f568c589306c32c12200bfb8843f
Akash Agrawall <akash.agrawall094@gmail.com>
2015-09-02 19:10:57 +0530

    Double/Triple hashing helps here

---
+++ codeforces/166div2/4.cpp
@@ -0,0 +1,110 @@
+/*******************
+  Akash Agrawall
+  IIIT HYDERABAD
+ ***********************/
+
+#include<bits/stdc++.h>
+using namespace std;
+#define FOR(i,a,b) for(i= a ; i < b ; ++i)
+#define rep(i,n) FOR(i,0,n)
+#define INF INT_MAX
+#define ALL(x) x.begin(),x.end()
+#define LET(x,a) __typeof(a) x(a)
+#define IFOR(i,a,b) for(LET(i,a);i!=(b);++i)
+#define EACH(it,v) IFOR(it,v.begin(),v.end())
+#define pb push_back
+#define sz(x) int(x.size())
+#define mp make_pair
+#define fill(x,v) memset(x,v,sizeof(x))
+#define max(a,b) ((a)>(b)?(a):(b))
+#define min(a,b) ((a)<(b)?(a):(b))
+#define si(n) scanf("%d",&n)
+#define pi(n) printf("%d ",n)
+#define pd(n) printf("%lf ",n);
+#define pdl(n) printf("%lf\n",n);
+#define pin(n) printf("%d\n",n)
+#define pln(n) printf("%lld\n",n)
+#define pl(n) printf("%lld ",n)
+#define sl(n) scanf("%lld",&n)
+#define sd(n) scanf("%lf",&n)
+#define ss(n) scanf("%s",n)
+#define scan(v,n) vector<int> v;rep(i,n){ int j;si(j);v.pb(j);}
+#define mod1 (ll)(1e13 + 61)
+#define mod2 (ll)(1e14 + 69)
+#define mod3 (ll)(1e15 + 61)
+#define ll long long int
+#define MAX 1000006
+#define TRACE
+
+#ifdef TRACE
+#define trace1(x)                cerr << #x << ": " << x << endl;
+#define trace2(x, y)             cerr << #x << ": " << x << " | " << #y << ": " << y << endl;
+#define trace3(x, y, z)          cerr << #x << ": " << x << " | " << #y << ": " << y << " | " << #z << ": " << z << endl;
+#define trace4(a, b, c, d)       cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << endl;
+#define trace5(a, b, c, d, e)    cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << " | " << #e << ": " << e << endl;
+#define trace6(a, b, c, d, e, f) cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << " | " << #e << ": " << e << " | " << #f << ": " << f << endl;
+
+#else
+
+#define trace1(x)
+#define trace2(x, y)
+#define trace3(x, y, z)
+#define trace4(a, b, c, d)
+#define trace5(a, b, c, d, e)
+#define trace6(a, b, c, d, e, f)
+
+#endif
+ll modpow(ll a,ll n,ll temp){ll res=1,y=a;while(n>0){if(n&1)res=(res*y)%temp;y=(y*y)%temp;n/=2;}return res%temp;} 
+char ch[MAX], c1[MAX];
+int cum[MAX], val[27];
+set<pair<int,pair<int,int> > > sit;
+int main()
+{
+	int v=0,sz,i,j,k,ans;
+	ll h1,h2,h3;
+	ss(ch);
+	ss(c1);
+	si(k);
+	sz=strlen(c1);
+	rep(i,sz)
+	{
+		val[i]=c1[i]-'0';
+		val[i]=1-val[i];
+	}
+	sz=strlen(ch);
+	rep(i,sz)
+	{
+		v+=val[ch[i]-'a'];
+		cum[i+1]=v;
+	}
+	rep(i,sz)
+	{
+		h1=h2=h3=0;
+		FOR(j,i,sz)
+		{
+			h1=h1*26+ch[j];
+			h2=h2*26+ch[j];
+			h3=h3*26+ch[j];
+			h1%=mod1; h2%=mod2; h3%=mod3;
+			if(cum[j+1]-cum[i]>k)
+				break;
+			sit.insert(mp(h1,mp(h2,h3)));
+		}
+	}
+	ans=sit.size();
+	pin(ans);
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
