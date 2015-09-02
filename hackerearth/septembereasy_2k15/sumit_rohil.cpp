e25b4be156bb4001cdf0204cae683ce0446a0e75
Akash Agrawall <akash.agrawall094@gmail.com>
2015-09-03 01:27:44 +0530

    Added all codes of september easy challenge HE 2k15

---
+++ hackerearth/septembereasy_2k15/sumit_rohil.cpp
@@ -0,0 +1,116 @@
+/*******************
+  Akash Agrawall
+  IIIT HYDERABAD
+ ***********************/
+
+#include<bits/stdc++.h>
+using namespace std;
+#define FOR(i,a,b) for(i= a ; i < b ; ++i)
+#define rep(i,n) FOR(i,0,n)
+#define INF INT_MAX
+#define ALL(x) x.begin(),x.end()
+#define LET(x,a) __typeof(a) x(a)
+#define IFOR(i,a,b) for(LET(i,a);i!=(b);++i)
+#define EACH(it,v) IFOR(it,v.begin(),v.end())
+#define pb push_back
+#define sz(x) int(x.size())
+#define mp make_pair
+#define fill(x,v) memset(x,v,sizeof(x))
+#define max(a,b) ((a)>(b)?(a):(b))
+#define min(a,b) ((a)<(b)?(a):(b))
+#define si(n) scanf("%d",&n)
+#define pi(n) printf("%d ",n)
+#define pd(n) printf("%lf ",n);
+#define pdl(n) printf("%lf\n",n);
+#define pin(n) printf("%d\n",n)
+#define pln(n) printf("%lld\n",n)
+#define pl(n) printf("%lld ",n)
+#define sl(n) scanf("%lld",&n)
+#define sd(n) scanf("%lf",&n)
+#define ss(n) scanf("%s",n)
+#define scan(v,n) vector<int> v;rep(i,n){ int j;si(j);v.pb(j);}
+#define F first
+#define S second
+#define mod (int)(1e9 + 7)
+#define ll long long int
+#define MAX 1000006
+#define TRACE
+
+#ifdef TRACE
+#define trace1(x)                cerr << #x << ": " << x << endl;
+#define trace2(x, y)             cerr << #x << ": " << x << " | " << #y << ": " << y << endl;
+#define trace3(x, y, z)          cerr << #x << ": " << x << " | " << #y << ": " << y << " | " << #z << ": " << z << endl;
+#define trace4(a, b, c, d)       cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << endl;
+#define trace5(a, b, c, d, e)    cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << " | " << #e << ": " << e << endl;
+#define trace6(a, b, c, d, e, f) cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << " | " << #e << ": " << e << " | " << #f << ": " << f << endl;
+
+#else
+
+#define trace1(x)
+#define trace2(x, y)
+#define trace3(x, y, z)
+#define trace4(a, b, c, d)
+#define trace5(a, b, c, d, e)
+#define trace6(a, b, c, d, e, f)
+
+#endif
+ll modpow(ll a,ll n,ll temp){ll res=1,y=a;while(n>0){if(n&1)res=(res*y)%temp;y=(y*y)%temp;n/=2;}return res%temp;} 
+char ch[444][444];
+int freq[102][27], visited[MAX];
+int equality_check(int v1, int v2)
+{
+	int i,s1,s2;
+	rep(i,27)
+	{
+		if(freq[v1][i]!=freq[v2][i])
+			return 0;
+	}
+	s1=strlen(ch[v1]); s2=strlen(ch[v2]);
+	if(ch[v1][0]==ch[v2][0] && ch[v1][s1-1]==ch[v2][s2-1])
+		return 1;
+	return 0;
+}
+set<int> stit;
+int main()
+{
+	int n,i,cnt=1,sz,j;
+	si(n);
+	rep(i,n)
+	{
+		ss(ch[i]);
+		sz=strlen(ch[i]);
+		rep(j,sz)
+			freq[i][ch[i][j]-'a']++;
+	}
+	rep(i,n)
+	{
+		if(visited[i])
+			continue;
+		visited[i]=cnt++;
+		rep(j,n)
+		{
+			if(j==i)
+				continue;
+			if(equality_check(j,i))
+				visited[j]=cnt-1;
+		}
+	}
+	rep(i,n)
+		stit.insert(visited[i]);
+	sz=stit.size();
+	pin(sz);
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
