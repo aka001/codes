6ce50f160d21c58b38515f3d80bc1f48e65ad897
Akash Agrawall <akash.agrawall094@gmail.com>
2015-09-10 18:39:13 +0530

    256div2 Problem C and D. Problem C is a good DP problem. Give it a try

---
+++ codeforces/256div2/3.cpp
@@ -0,0 +1,119 @@
+/*******************
+  Akash Agrawall
+  IIIT HYDERABAD
+ ***********************/
+
+#include<bits/stdc++.h>
+using namespace std;
+#define FOR(i,a,b) for(i= a ; i < b ; ++i)
+#define rep(i,n) FOR(i,0,n)
+#define INF INT_MAX
+#define ALL(x) x.begin(),x.end()
+#define LET(x,a) __typeof(a) x(a)
+#define IFOR(i,a,b) for(LET(i,a);i!=(b);++i)
+#define EACH(it,v) IFOR(it,v.begin(),v.end())
+#define pb push_back
+#define sz(x) int(x.size())
+#define mp make_pair
+#define fill(x,v) memset(x,v,sizeof(x))
+#define max(a,b) ((a)>(b)?(a):(b))
+#define min(a,b) ((a)<(b)?(a):(b))
+#define si(n) scanf("%d",&n)
+#define pi(n) printf("%d ",n)
+#define pd(n) printf("%lf ",n);
+#define pdl(n) printf("%lf\n",n);
+#define pin(n) printf("%d\n",n)
+#define pln(n) printf("%lld\n",n)
+#define pl(n) printf("%lld ",n)
+#define sl(n) scanf("%lld",&n)
+#define sd(n) scanf("%lf",&n)
+#define ss(n) scanf("%s",n)
+#define scan(v,n) vector<int> v;rep(i,n){ int j;si(j);v.pb(j);}
+#define mod (int)(1e9 + 7)
+#define ll long long int
+#define MAX 5006
+#define TRACE
+
+#ifdef TRACE
+#define trace1(x)                cerr << #x << ": " << x << endl;
+#define trace2(x, y)             cerr << #x << ": " << x << " | " << #y << ": " << y << endl;
+#define trace3(x, y, z)          cerr << #x << ": " << x << " | " << #y << ": " << y << " | " << #z << ": " << z << endl;
+#define trace4(a, b, c, d)       cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << endl;
+#define trace5(a, b, c, d, e)    cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << " | " << #e << ": " << e << endl;
+#define trace6(a, b, c, d, e, f) cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << " | " << #e << ": " << e << " | " << #f << ": " << f << endl;
+
+#else
+
+#define trace1(x)
+#define trace2(x, y)
+#define trace3(x, y, z)
+#define trace4(a, b, c, d)
+#define trace5(a, b, c, d, e)
+#define trace6(a, b, c, d, e, f)
+
+#endif
+ll modpow(ll a,ll n,ll temp){ll res=1,y=a;while(n>0){if(n&1)res=(res*y)%temp;y=(y*y)%temp;n/=2;}return res%temp;} 
+ll dp[MAX][MAX], arr[MAX];
+ll solveit(ll i1, ll j1)
+{
+	ll &ret=dp[i1][j1];
+	if(ret!=-1)
+		return ret;
+	else if(i1==j1)
+	{
+		return min(1, arr[i1]);
+	}
+	else
+	{
+		ll mit=mod, ans=0,l,i,sz;
+		vector<ll> val;
+		FOR(i,i1,j1+1)
+			mit=min(mit, arr[i]);
+		FOR(i,i1,j1+1)
+		{
+			arr[i]-=mit;
+			if(arr[i]==0)
+				val.pb(i);
+		}
+		val.pb(j1+1);
+		sz=val.size();
+		l=i1;
+		rep(i,sz)
+		{
+			if(val[i]-1>=l)
+			{
+				ans+=solveit(l, val[i]-1);
+			}
+			l=val[i]+1;
+		}
+		ans+=mit;
+		ans=min(ans, j1-i1+1);
+	//	trace4(i1, j1, ans, mit);
+		ret=ans;
+		return ret;
+	}
+}
+int main()
+{
+	memset(dp, -1, sizeof(dp));
+	ll i,n,ans;
+	sl(n);
+	rep(i,n)
+		sl(arr[i]);
+	ans = solveit(0,n-1);
+	pln(ans);
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
