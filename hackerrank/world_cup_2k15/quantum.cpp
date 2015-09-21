42f88c20796a71aa92b9bbc0c9aa086089129272
Akash Agrawall <akash.agrawall094@gmail.com>
2015-09-21 23:19:42 +0530

    Added world-cup contest Hackerrank

---
+++ hackerrank/world_cup_2k15/quantum.cpp
@@ -0,0 +1,113 @@
+/*******************
+  Akash Agrawall
+  IIIT HYDERABAD
+ ***********************/
+
+#include<bits/stdc++.h>
+using namespace std;
+#define FOR(i,a,b) for(i= a ; i < b ; ++i)
+#define rep(i,n) FOR(i,0,n)
+#define INF INT_MAX
+#define ALL(x) x.begin(),x.end()
+#define LET(x,a) __typeof(a) x(a)
+#define IFOR(i,a,b) for(LET(i,a);i!=(b);++i)
+#define EACH(it,v) IFOR(it,v.begin(),v.end())
+#define pb push_back
+#define sz(x) int(x.size())
+#define mp make_pair
+#define fill(x,v) memset(x,v,sizeof(x))
+#define max(a,b) ((a)>(b)?(a):(b))
+#define min(a,b) ((a)<(b)?(a):(b))
+#define si(n) scanf("%d",&n)
+#define pi(n) printf("%d ",n)
+#define pd(n) printf("%lf ",n);
+#define pdl(n) printf("%lf\n",n);
+#define pin(n) printf("%d\n",n)
+#define pln(n) printf("%lld\n",n)
+#define pl(n) printf("%lld ",n)
+#define sl(n) scanf("%lld",&n)
+#define sd(n) scanf("%lf",&n)
+#define ss(n) scanf("%s",n)
+#define scan(v,n) vector<int> v;rep(i,n){ int j;si(j);v.pb(j);}
+#define F first
+#define S second
+#define mod (ll)(1e9 + 7)
+#define ll long long int
+#define MAX 100006
+#define TRACE
+
+#ifdef TRACE
+#define trace1(x)                cerr << #x << ": " << x << endl;
+#define trace2(x, y)             cerr << #x << ": " << x << " | " << #y << ": " << y << endl;
+#define trace3(x, y, z)          cerr << #x << ": " << x << " | " << #y << ": " << y << " | " << #z << ": " << z << endl;
+#define trace4(a, b, c, d)       cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << endl;
+#define trace5(a, b, c, d, e)    cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << " | " << #e << ": " << e << endl;
+#define trace6(a, b, c, d, e, f) cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << " | " << #e << ": " << e << " | " << #f << ": " << f << endl;
+
+#else
+
+#define trace1(x)
+#define trace2(x, y)
+#define trace3(x, y, z)
+#define trace4(a, b, c, d)
+#define trace5(a, b, c, d, e)
+#define trace6(a, b, c, d, e, f)
+
+#endif
+ll modpow(ll a,ll n,ll temp){ll res=1,y=a;while(n>0){if(n&1)res=(res*y)%temp;y=(y*y)%temp;n/=2;}return res%temp;} 
+vector<pair<int,double> > arr[MAX];
+double ans=0, vit[MAX];
+int visited[MAX];
+void dfs(int ind, double val, int comp)
+{
+	if(visited[ind]==comp)
+	{
+		double calc = val/(vit[ind]*1.0);
+		ans+=calc;
+		//trace5(ind, val, vit[ind],calc,ans);
+		return;
+	}
+	else if(visited[ind])
+		return;
+	vit[ind]=val;
+	visited[ind]=comp;
+	int i,sz;
+	sz=arr[ind].size();
+	rep(i,sz)
+	{
+		dfs(arr[ind][i].F, val*arr[ind][i].S, comp);
+	}
+}
+int main()
+{
+	int n,i,v1;
+	double val;
+	si(n);
+	FOR(i,1,n+1)
+	{
+		si(v1);
+		sd(val);
+		val/=100.0;
+		arr[i].pb(mp(v1,val));
+	}
+	FOR(i,1,n+1)
+	{
+		if(visited[i]==0)
+			dfs(i,1,i);
+	}
+	printf("%.2lf\n",ans);
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ hackerrank/world_cup_2k15/sum-nim.cpp
@@ -0,0 +1,120 @@
+/*******************
+  Akash Agrawall
+  IIIT HYDERABAD
+ ***********************/
+
+#include<bits/stdc++.h>
+using namespace std;
+#define FOR(i,a,b) for(i= a ; i < b ; ++i)
+#define rep(i,n) FOR(i,0,n)
+#define INF INT_MAX
+#define ALL(x) x.begin(),x.end()
+#define LET(x,a) __typeof(a) x(a)
+#define IFOR(i,a,b) for(LET(i,a);i!=(b);++i)
+#define EACH(it,v) IFOR(it,v.begin(),v.end())
+#define pb push_back
+#define sz(x) int(x.size())
+#define mp make_pair
+#define fill(x,v) memset(x,v,sizeof(x))
+#define max(a,b) ((a)>(b)?(a):(b))
+#define min(a,b) ((a)<(b)?(a):(b))
+#define si(n) scanf("%d",&n)
+#define pi(n) printf("%d ",n)
+#define pd(n) printf("%lf ",n);
+#define pdl(n) printf("%lf\n",n);
+#define pin(n) printf("%d\n",n)
+#define pln(n) printf("%lld\n",n)
+#define pl(n) printf("%lld ",n)
+#define sl(n) scanf("%lld",&n)
+#define sd(n) scanf("%lf",&n)
+#define ss(n) scanf("%s",n)
+#define scan(v,n) vector<int> v;rep(i,n){ int j;si(j);v.pb(j);}
+#define F first
+#define S second
+#define mod (ll)(1e9 + 7)
+#define ll long long int
+#define MAX 1000006
+#define TRACE
+
+#ifdef TRACE
+#define trace1(x)                cerr << #x << ": " << x << endl;
+#define trace2(x, y)             cerr << #x << ": " << x << " | " << #y << ": " << y << endl;
+#define trace3(x, y, z)          cerr << #x << ": " << x << " | " << #y << ": " << y << " | " << #z << ": " << z << endl;
+#define trace4(a, b, c, d)       cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << endl;
+#define trace5(a, b, c, d, e)    cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << " | " << #e << ": " << e << endl;
+#define trace6(a, b, c, d, e, f) cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << " | " << #e << ": " << e << " | " << #f << ": " << f << endl;
+
+#else
+
+#define trace1(x)
+#define trace2(x, y)
+#define trace3(x, y, z)
+#define trace4(a, b, c, d)
+#define trace5(a, b, c, d, e)
+#define trace6(a, b, c, d, e, f)
+
+#endif
+ll modpow(ll a,ll n,ll temp){ll res=1,y=a;while(n>0){if(n&1)res=(res*y)%temp;y=(y*y)%temp;n/=2;}return res%temp;} 
+ll arr[40], dp[40][100005], c=0,n;
+map<ll,ll> mapit;
+ll compute(ll ind, ll val)
+{
+	ll cit;
+	if(mapit.find(val)!=mapit.end())
+	{
+		ll aka;
+		aka=mapit[val];
+		ll &ret = dp[ind][aka];
+		if(ret!=-1)
+			return ret;
+		cit=mapit[val];
+	}
+	else
+	{
+		mapit[val]=c;
+		cit=c;
+		c++;
+	}
+	assert(cit<=100005);
+	ll &ret = dp[ind][cit];
+	if(ind==n && val==0)
+		return ret=1;
+	else if(ind==n)
+		return ret=0;
+	else
+	{
+		ll sum=0,ans=0,i;
+		FOR(i,ind,n)
+		{
+			sum+=arr[i];
+			ans+=compute(i+1, sum^val);
+		}
+		//trace3(ind, val, ans);
+		return ret=ans;
+	}
+}
+int main()
+{
+	fill(dp, -1);
+	ll i,j,calc,sum,flag,fin=0,vit;
+	ll cnt=0; calc=0;
+	sl(n);
+	rep(i,n)
+		sl(arr[i]);
+	fin=compute(0,0);
+	pln(fin);
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
