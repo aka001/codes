cfeb1dca12c6a7a1a80402b41ea95b830b150ad4
Akash Agrawall <akash.agrawall094@gmail.com>
2015-09-24 02:05:18 +0530

    Added 101 Hack Sept

---
+++ hackerrank/101hacksept_2k15/2.cpp
@@ -0,0 +1,128 @@
+/*******************
+  Akash Agrawall
+  IIIT HYDERABAD
+ ***********************/
+
+#include<bits/stdc++.h>
+using namespace std;
+#define FOR(i,a,b) for(i= a ; i < b ; ++i)
+#define rep(i,n) FOR(i,0,n)
+#define INF INT_MAX
+#define ALL(x) x.begin(),x.end()
+#define LET(x,a) __typeof(a) x(a)
+#define IFOR(i,a,b) for(LET(i,a);i!=(b);++i)
+#define EACH(it,v) IFOR(it,v.begin(),v.end())
+#define pb push_back
+#define sz(x) int(x.size())
+#define mp make_pair
+#define fill(x,v) memset(x,v,sizeof(x))
+#define max(a,b) ((a)>(b)?(a):(b))
+#define min(a,b) ((a)<(b)?(a):(b))
+#define si(n) scanf("%d",&n)
+#define pi(n) printf("%d ",n)
+#define pd(n) printf("%lf ",n);
+#define pdl(n) printf("%lf\n",n);
+#define pin(n) printf("%d\n",n)
+#define pln(n) printf("%lld\n",n)
+#define pl(n) printf("%lld ",n)
+#define sl(n) scanf("%lld",&n)
+#define sd(n) scanf("%lf",&n)
+#define ss(n) scanf("%s",n)
+#define scan(v,n) vector<int> v;rep(i,n){ int j;si(j);v.pb(j);}
+#define F first
+#define S second
+#define mod (ll)(1e9 + 7)
+#define ll long long int
+#define MAX 600006
+#define TRACE
+
+#ifdef TRACE
+#define trace1(x)                cerr << #x << ": " << x << endl;
+#define trace2(x, y)             cerr << #x << ": " << x << " | " << #y << ": " << y << endl;
+#define trace3(x, y, z)          cerr << #x << ": " << x << " | " << #y << ": " << y << " | " << #z << ": " << z << endl;
+#define trace4(a, b, c, d)       cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << endl;
+#define trace5(a, b, c, d, e)    cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << " | " << #e << ": " << e << endl;
+#define trace6(a, b, c, d, e, f) cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << " | " << #e << ": " << e << " | " << #f << ": " << f << endl;
+
+#else
+
+#define trace1(x)
+#define trace2(x, y)
+#define trace3(x, y, z)
+#define trace4(a, b, c, d)
+#define trace5(a, b, c, d, e)
+#define trace6(a, b, c, d, e, f)
+
+#endif
+ll modpow(ll a,ll n,ll temp){ll res=1,y=a;while(n>0){if(n&1)res=(res*y)%temp;y=(y*y)%temp;n/=2;}return res%temp;} 
+vector<int> arr[MAX];
+int val[MAX], sz[MAX], cit[MAX],ans=0,visited[MAX];
+set<pair<int,int> > sit;
+void dfs(int node, int st)
+{
+	visited[node]=1;
+	int i,sz,flag=1,chld;
+	sz=arr[node].size();
+	//trace3(node, sz, st);
+	if(sz==1 && node!=st)
+		return;
+	rep(i,sz)
+	{
+		chld=arr[node][i];
+		//trace2(node, chld);
+		if(visited[chld])
+			continue;
+		dfs(chld, st);
+		if(val[chld]!=1)
+		{
+			flag=0;
+		}
+	}
+	//trace3(node, val[node], flag);
+	if(!(flag) && val[node]==0)
+	{
+		//trace1("hie");
+		val[node]=1;
+		ans++;
+	}
+}
+int main()
+{
+	int t,i,j,chld,n,v1,v2,cnt;
+	si(t);
+	while(t--)
+	{
+		fill(visited, 0);
+		sit.clear();
+		ans=0;
+		si(n);
+		rep(i,n+1)
+			arr[i].clear();
+		rep(i,n)
+			si(val[i+1]);
+		rep(i,n-1)
+		{
+			si(v1); si(v2);
+			arr[v1].pb(v2);
+			arr[v2].pb(v1);
+		}
+		dfs(1,1);
+		if(n==1 && val[1]!=1)
+			ans=1;
+		pin(ans);
+	}
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
