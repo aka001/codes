6b404cc26eb0f86b9bcf1138ac61bcf0ca664aa0
Akash Agrawall <akash.agrawall094@gmail.com>
2015-09-27 19:56:19 +0530

    Added September Lunchtime. Upsolving still in process

---
+++ codechef/september_lunchtime_2k15/coffee.cpp
@@ -0,0 +1,97 @@
+/*******************
+  Akash Agrawall
+  IIIT HYDERABAD
+ ***********************/
+
+#include<bits/stdc++.h>
+using namespace std;
+#define FOR(i,a,b) for(i= a ; i < b ; ++i)
+#define rep(i,n) FOR(i,0,n)
+#define INF INT_MAX
+#define ALL(x) x.begin(),x.end()
+#define LET(x,a) __typeof(a) x(a)
+#define IFOR(i,a,b) for(LET(i,a);i!=(b);++i)
+#define EACH(it,v) IFOR(it,v.begin(),v.end())
+#define pb push_back
+#define sz(x) int(x.size())
+#define mp make_pair
+#define fill(x,v) memset(x,v,sizeof(x))
+#define max(a,b) ((a)>(b)?(a):(b))
+#define min(a,b) ((a)<(b)?(a):(b))
+#define si(n) scanf("%d",&n)
+#define pi(n) printf("%d ",n)
+#define pd(n) printf("%lf ",n);
+#define pdl(n) printf("%lf\n",n);
+#define pin(n) printf("%d\n",n)
+#define pln(n) printf("%lld\n",n)
+#define pl(n) printf("%lld ",n)
+#define sl(n) scanf("%lld",&n)
+#define sd(n) scanf("%lf",&n)
+#define ss(n) scanf("%s",n)
+#define scan(v,n) vector<int> v;rep(i,n){ int j;si(j);v.pb(j);}
+#define F first
+#define S second
+#define mod (ll)(1e9 + 7)
+#define ll long long int
+#define MAX 1000006
+#define TRACE
+
+#ifdef TRACE
+#define trace1(x)                cerr << #x << ": " << x << endl;
+#define trace2(x, y)             cerr << #x << ": " << x << " | " << #y << ": " << y << endl;
+#define trace3(x, y, z)          cerr << #x << ": " << x << " | " << #y << ": " << y << " | " << #z << ": " << z << endl;
+#define trace4(a, b, c, d)       cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << endl;
+#define trace5(a, b, c, d, e)    cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << " | " << #e << ": " << e << endl;
+#define trace6(a, b, c, d, e, f) cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << " | " << #e << ": " << e << " | " << #f << ": " << f << endl;
+
+#else
+
+#define trace1(x)
+#define trace2(x, y)
+#define trace3(x, y, z)
+#define trace4(a, b, c, d)
+#define trace5(a, b, c, d, e)
+#define trace6(a, b, c, d, e, f)
+
+#endif
+ll modpow(ll a,ll n,ll temp){ll res=1,y=a;while(n>0){if(n&1)res=(res*y)%temp;y=(y*y)%temp;n/=2;}return res%temp;} 
+ll dp[215][215][215],arr[215],n,k,d,m,mit;
+ll compute(ll ind, ll coffee, ll lastit)
+{
+	ll &ret = dp[ind][coffee][lastit];
+	if(ret!=-1)
+		return ret;
+	else if(ind==n+1)
+	{
+		if(coffee==k)
+			return ret=0;
+		else
+			return ret=-(1LL<<50);
+	}
+	else
+	{
+		ll ans=0;
+		if(coffee<k)
+			ans=compute(ind+1, coffee+1, ind);
+		if(lastit!=213 && ind-lastit<=d)
+			ans=max(ans, m*arr[ind]+compute(ind+1, coffee, lastit));
+		else
+			ans=max(ans, arr[ind]+compute(ind+1, coffee, lastit));
+		return ret=ans;
+	}
+}
+int main()
+{
+	ll t, i, j, l, ans;
+	sl(t);
+	while(t--)
+	{
+		sl(n); sl(k); sl(d); sl(m);
+		fill(dp, -1);
+		rep(i,n)
+			sl(arr[i+1]);
+		ans = compute(1, 0, 213);
+		pln(ans);
+	}
+	return 0;
+}
